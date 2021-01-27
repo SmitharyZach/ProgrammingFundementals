88913fa735cdfdd39968af69ee1accca873602b3
Zach Smith <znathanielsmith@gmail.com>
2021-01-27 07:38:40 -0500

    updates hw

--- express-reading/index.js
+++ express-reading/index.js
@@ -1,15 +1,44 @@
 const http = require("http");
 
 const hostname = "127.0.0.1";
-const port = 3001;
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  const { url } = req;
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+const express = require("express");
+const app = express();
+
+const server = http.createServer(app);
+const db = require("./db");
+
+console.log(db);
+app.get("/", (req, res) => {
+  res.send("hello from express");
+});
+
+app.get("/friends/", (req, res) => {
+  let html = ``;
+  html += `<ul>`;
+  db.forEach((friend) => {
+    html += `<li>
+                <a href="${req.path}${friend.handle}">${friend.name}</a>
+              </li>`;
+  });
+  html += `</ul>`;
+  res.send(html);
+});
+
+app.get("/friends/:handle", (req, res) => {
+  const { handle } = req.params;
+  const friend = db.find((friend) => friend.handle === handle);
+  if (friend) {
+    res.send(`
+    <h1>${friend.name}</h1>
+    <h3>${friend.handle}</h3>
+    <h3>${friend.skill}</h3>`);
+  } else {
+    res.status(404).send(`No user with the handle ${handle}`);
+  }
 });
 
 server.listen(port, hostname, () => {
-  console.log("heyo");
+  console.log(`Server running HOT`);
 });
